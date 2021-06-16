444479881a6f0798638471dd2a14e8aa61ed192d
Hoang Hao <hoanghao18101999@gmail.com>
2021-06-16 13:23:22 +0700

    delete https

--- src/services/user.js
+++ src/services/user.js
@@ -3,7 +3,7 @@ export const UserService = {
     login: ({
         PhoneNumber,
         Password
-    }) => axios.post('https://207.148.69.136/v1/users/login',{
+    }) => axios.post('http://207.148.69.136/v1/users/login',{
         PhoneNumber,
         Password
     })
